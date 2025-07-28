3b04236cee7c36261034455302017f5e71c21e6d
Wertasile <am8704y@gre.ac.uk>
2025-07-28 15:00:56 +0300

    HomePage all sections added

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState} from 'react'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom'
 import { useHistory, Link } from 'react-router-dom/cjs/react-router-dom.min'
+import FAQs from '../components/miscallaneous/FAQs.json'
 
 const Homepage = () => {
+    const [expand, setExpand] = useState(new Set())
     const history = useHistory()
 
+    const toggleExpand = (index) => {
+        setExpand(prev => {
+            const copySet = new Set(prev)
+
+            if (copySet.has(index)){
+                copySet.delete(index)
+            } else {
+                copySet.add(index)
+            }
+            return copySet
+        })
+    }
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem("userInfo"))
 
@@ -16,7 +30,7 @@ const Homepage = () => {
     return (
         <>
             <header>
-                <div style={{display:'flex'}}><img src='/images/Troupe_Logo.png' width="32"/><span>TROUPE</span></div>
+                <div style={{display:'flex'}}><img src='/images/Troupe_Logo.png' width="40"/></div>
                 <nav>
                     <NavLink to="/login">Discover</NavLink>
                     <NavLink to="/login">Blog</NavLink>
@@ -27,6 +41,7 @@ const Homepage = () => {
             </header>
 
             <main>
+
                 <section id="hero">
                     <div className='banner'>
                         <h1>Welcome to THE Communication Platform to</h1>
@@ -35,6 +50,7 @@ const Homepage = () => {
                     </div>
                     
                 </section>
+
                 <section id='features'>
                     <div className='banner'>
                         <h1>Communicating through multiple means</h1>
@@ -72,20 +88,61 @@ const Homepage = () => {
 
                     </div>
                 </section>
+
                 <section id="pricing">
-                    
+                    <div className='price-cards'>
+                        <div>
+                            <h1>Pricing plans, for everyone!</h1>
+                            <p>Well, this is here just to say that this application is completely free!</p>
+                        </div>
+                        <div className='card'>
+                            <h2>For Individuals, Start for free</h2>
+                            <div>Get access to:</div>
+                            <ul>
+                                <li><i class="fa-solid fa-circle-check" style={{color: '#B197FC'}}></i> DMs and Group Chats</li>
+                                <li><i class="fa-solid fa-circle-check" style={{color: '#B197FC'}}></i> Multimedia sharing</li>
+                                <li><i class="fa-solid fa-circle-check" style={{color: '#B197FC'}}></i> Voice messaging</li>
+                            </ul>
+                            <NavLink to="login" className="primary-btn" style={{width:'200px'}}>Start Building</NavLink>
+                        </div>
+                        <div className='card'>
+                            <h2>Enterprise</h2>
+                            <h1>Pricing plans based on use</h1>
+                            <div>
+                                Upgrade as per your requirements. Be selective, for the features that you want, that you pay for. More support, More features and much much More.
+                            </div>
+                            <NavLink to="login" className="primary-btn" style={{width:'200px'}}>Start Building</NavLink>
+                            
+                        </div>
+
+                    </div>
                 </section>
+
                 <section id="FAQs">
+                    <div style={{alignItems:'left'}}>
+                        <h1>FAQs</h1>
+                        {FAQs.map((FAQ, index) => 
+                            (<div style={{borderBottom:'0.5px gray solid', padding:'10px'}}>
+                                <div onClick={() => {toggleExpand(index)}} key={index} >
+                                    <h2>{FAQ.title} {expand.has(index) ? (<i class="fa-solid fa-chevron-down"></i>) :(<i class="fa-solid fa-chevron-up"></i>)} </h2>
+                                </div>
+                                <div>
+                                    {expand.has(index) && <div>{FAQ.desc}</div>}
+                                </div>
+                            </div>) 
+                        )}
+                    </div>
                     
                 </section>
             </main>
 
             <footer>
                 <div id='footer-left'>
-                    <div style={{display:'flex'}}><img src='/images/Troupe_Logo.png' width="48"/><span>TROUPE</span></div>
-                    <div>Base44 is the AI-powered platform that lets users build fully functioning apps in minutes. Using nothing but natural language, Base44 enables anyone to turn their words into personal productivity apps, back-office tools, customer portals, or complete enterprise products that are ready to use, no integrations required.</div>
+                    <div style={{display:'flex', alignItems:'center'}}><img src='/images/Troupe_Logo.png' width="48"/><span> <h2>TROUPE</h2></span></div>
+                    <div style={{textAlign:'justify'}}>Troupe is basically a texting platform in its base. It is supported by features, mainly multimedia which provides a way to share things through multiple means.</div>
                     <div style={{display:'flex', gap:"10px"}}>
                         <i class="fa-brands fa-discord"></i>
+                        <i class="fa-brands fa-github"></i>
                         <i class="fa-brands fa-linkedin-in"></i>
                         <i class="fa-solid fa-envelope"></i>
                         <i class="fa-solid fa-briefcase"></i>
